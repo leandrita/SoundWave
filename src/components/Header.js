deaf21a3dc011494bf05c958e8e0ee2e6bdde043
Rita7979 <131756045+Rita7979@users.noreply.github.com>
2023-06-16 15:12:54 +0200

    img,nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div>
+     <img/>
+     <nav>
+        <ul>
+          <li>Discover</li>
+          <li>Join</li>
+        </ul>
+      </nav> 
+    </div>
   )
 }
