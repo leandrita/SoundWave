f0c1dfd51191f1c11d66317046d41ccfb1afb6a8
Neema <106617302+shedevopsbcn@users.noreply.github.com>
2023-06-19 10:11:38 +0200

    agrego carpeta css

--- src/components/MainIndex.js
+++ src/components/MainIndex.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Girl from '../assets/landing-page-girl.png';
+import "../css/MainIndex.css";
 
 export default function MainIndex() {
   return (
-    <div>
+    <div className='main'>
       <img src={Girl}/>
       <h1>Feel The Music</h1>
       <p>Stream over 20 thousand songs with one click</p>
