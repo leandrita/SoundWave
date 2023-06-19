a7b95fb62d8c836def7c7622c61d54c034115161
leandrita <lea.bujh@gmail.com>
2023-06-19 09:15:28 +0200

    MainIndex

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Main from './components/Main';
+import MainIndex from './components/MainIndex';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -7,9 +7,9 @@ import Footer from './components/Footer';
 function App() {
   return (
     <body>
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Header />
+      <MainIndex />
+      <Footer />
     </body>
   );
 }
---
+++ src/components/MainIndex.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from './Form';
+
+export default function MainIndex() {
+  return (
+    <div>
+      <img></img>
+      <h1>Feel The Music</h1>
+      <p>Stream over 20 thousand songs with one click</p>
+      <button>Join Now</button>
+    </div>
+  )
+}
