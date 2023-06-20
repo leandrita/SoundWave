3ddb834c9fb88972b875025b6bba053fa79d02bb
unknown <milehalasj@gmail.com>
2023-06-20 12:03:12 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import twitter from '../assets/twitter.svg'
+import facebook from '../assets/facebook.svg'
+import "../css/Footer.css"
 
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+      <div className='container'>
+        <div className='footerRight'>
+           <p>About Us</p>
+           <p>Contat</p>
+        </div>
+        <div className='footerLeft'>
+            <img src={twitter}/>
+            <p className='twitter'>Twitter</p>
+            <img src={facebook}/>
+            <p className='facebook'>Facebook</p>
+          
+        </div>
+      </div>
+    </footer>
   )
 }
+
