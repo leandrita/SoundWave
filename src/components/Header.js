c926e0029ae511fe438952ee39283debe9375db7
leandrita <lea.bujh@gmail.com>
2023-06-23 10:57:52 +0200

    styles main index

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,28 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import React from 'react';
 import Logo from '../assets/logo.png';
 import '../css/Header.css';
 
 
 export default function Header() {
+  const linkStyle = {
+    textDecoration: 'none'
+  };
   return (
     <>
       <div className='Header'>
-        <Link to="/"><div className='LogoContainer'>
+        <Link to="/" style={linkStyle}><div className='LogoContainer'>
           <img src={Logo} className='Logo' alt='Imagen del Logo' />
           <span className='ParrafoLogo'>Soundwave</span>
         </div></Link>
         <nav>
           <ul>
-            <Link to="/discover"><li>Discover</li></Link>
-            <Link to="/join"><li>Join</li></Link>
+            <Link to="/discover" style={linkStyle}><li>Discover</li></Link>
+            <Link to="/join" style={linkStyle}><li>Join</li></Link>
           </ul>
         </nav>
-        {/* <Link to="/inexistente">Error 404</Link> */}
       </div>
-      {/* <div className='Header'>
-        <NavLink className={({ isActive }) => (isActive ? "active-link" : null)} to="/"><div className='LogoContainer'>
-          <img src={Logo} className='Logo' alt='Imagen del Logo' />
-          <span className='ParrafoLogo'>Soundwave</span>
-        </div></NavLink>
-        <nav>
-          <ul>
-            <NavLink className={({ isActive }) => (isActive ? "active-link" : null)} to="discover"><li>Discover</li></NavLink>
-            <NavLink className={({ isActive }) => (isActive ? "active-link" : null)} to="join"><li>Join</li></NavLink>
-          </ul>
-        </nav>
-      </div> */}
+
     </>
   );
 }
--- src/components/MainIndex.js
+++ src/components/MainIndex.js
@@ -5,7 +5,13 @@ import "../css/MainIndex.css";
 
 export default function MainIndex() {
   return (
-    <div className='main'> 
+    <div className='main'>
+      <div className='circleViolet'></div>
+      <div className='circlePurple'></div>
+      <div className="circlebigViolet"></div>
+      <div className='imageGirl'>
+        <img src={Girl} alt='girl'></img>
+      </div>
       <div className='content-main'>
         <h1 className='first-content'>Feel The Music</h1>
         <p className='second-content'>
@@ -13,12 +19,6 @@ export default function MainIndex() {
           Click
         </p>
         <Link to="/join"><button className='container-button'>Join Now</button></Link>
-      </div>
-      <div className='circleViolet'></div>
-      <div className='circlePurple'></div>
-      <div className="circlebigViolet"></div>
-      <div className='image'>
-        <img src={Girl} alt='girl'></img>
       </div>
     </div>
   )
